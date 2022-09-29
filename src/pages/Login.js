93883e44a159b49aff30a0743add8328880f275b
caren <carenoliveirasilvapontes@gmail.com>
2022-09-29 16:56:38 -0500

    requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getEmail } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+
+    email: '',
+    password: '',
+    isDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => this.verifyBtn());
+  };
+
+  verifyBtn = () => {
+    const { email, password } = this.state;
+    const regex = /\S+@\S+\.\S+/;
+    const numberMin = 6;
+    const verifyEmail = regex.test(email);
+    const verifypassword = password.length >= numberMin;
+    this.setState({ isDisabled: !(verifyEmail && verifypassword) });
+  };
+
+  handleClick = (e) => {
+    e.preventDefault();
+    const { history, dispatch } = this.props;
+    const { email } = this.state;
+    dispatch(getEmail(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { isDisabled } = this.state;
+    return (
+      <form>
+        <input
+          type="email"
+          data-testid="email-input"
+          name="email"
+          onChange={ this.handleChange }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          name="password"
+          onChange={ this.handleChange }
+        />
+        <button type="submit" disabled={ isDisabled } onClick={ this.handleClick }>
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
-export default Login;
+export default connect()(Login);
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,18 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { GET_EMAIL } from '../actions';
+
+const initialState = {
+  email: '',
+};
+
+function user(state = initialState, action) {
+  switch (action.type) {
+  case GET_EMAIL:
+    return {
+      ...state,
+      email: action.payload,//olhar sempre o nome da chave quee coloquei em action
+    };
+  default:
+    return state;
+  }
+}
+export default user;
